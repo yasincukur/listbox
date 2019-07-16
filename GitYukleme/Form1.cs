4056c111cf2333ad2cd10f5ef0c23e0a792c97df
yasincukur <cukurr@hotmail.com>
2019-07-16 23:17:13 +0300

    yasin cukur

--- GitYukleme/Form1.cs
+++ GitYukleme/Form1.cs
@@ -16,5 +16,57 @@ namespace GitYukleme
         {
             InitializeComponent();
         }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string veri = textBox1.Text;
+            listBox1.Items.Add(veri);
+            textBox1.Focus();
+            textBox1.Clear();
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            { 
+            listBox2.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItem != null)
+            {
+                listBox1.Items.Add(listBox2.SelectedItem);
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] dizi = { "yasin", "furkan", "sedat", "fatih", "murat", "selim", };
+            foreach (string item in dizi)
+            {
+                listBox1.Items.Add(item);
+
+            }
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null )
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            else if (listBox2.SelectedItem != null )
+            {
+                listBox2.Items.RemoveAt(listBox2.SelectedIndex);
+            }
+        }
     }
 }
